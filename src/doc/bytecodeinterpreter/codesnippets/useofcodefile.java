81a925c02b5e7b0789729832cc2f9518baf31b3d
Martin Steffen <msteffen@ifi.uio.no>
2019-02-12 15:16:18 +0100

    OK

--- src/doc/bytecodeinterpreter/codesnippets/useofcodefile.java
+++ src/doc/bytecodeinterpreter/codesnippets/useofcodefile.java
@@ -1,2 +1,12 @@
 CodeFile codeFile = new CodeFile();
 String filename = "example.bin";
+
+// Building the bytecode with instructions like
+codeFile.addProcedure("Main");
+CodeProcedure main = new CodeProcedure("Main",
+				       VoidType.TYPE,
+				       codeFile);
+main.addInstruction (new RETURN());
+codeFile.updateProcedure(main);
+
+// ... and more ....
