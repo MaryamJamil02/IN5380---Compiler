8437668e7eb8434cb9c200cfc11609846cf3c62b
Martin Steffen <msteffen@ifi.uio.no>
2021-03-22 10:05:36 +0100

    --

--- src/bytecode/instructions/ADD.java
+++ src/bytecode/instructions/ADD.java
@@ -3,13 +3,14 @@ package bytecode.instructions;
 import runtime.Interpreter;
 
 public class ADD extends Instruction{
-	public byte[] getBytecode() {
-		return new byte[]{1};
-	}
-	public int size() {
-		return 1;
-	}
-	public String toString(Interpreter interpreter) {
-		return "add";
-	}
+    public byte[] getBytecode() {
+	return new byte[]{1};
+    }
+    public int size() {
+	return 1;
+    }
+    public String toString(Interpreter interpreter) {
+	return "add";
+    }
 }
+
