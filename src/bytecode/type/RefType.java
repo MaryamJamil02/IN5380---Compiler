1e18f62e22099e164658eee59a2b26d26f68272c
Martin Steffen <msteffen@ifi.uio.no>
2021-03-29 10:40:19 +0200

    --

--- src/bytecode/type/RefType.java
+++ src/bytecode/type/RefType.java
@@ -5,19 +5,19 @@ import runtime.Interpreter;
 import runtime.NullReference;
 
 public class RefType extends CodeType{
-	private int structRef;
-	public RefType(int structRef) {
-		this.structRef = structRef;
-	}
-	public byte[] getBytecode() {
-		byte[] res = new byte[]{1, 0, 0};
-		NumberConversion.shortToByteArray(res, 1, (short)this.structRef);
-		return res;
-	}
-	public String toString(Interpreter interpreter) {
-		return "" + interpreter.structs[this.structRef].name + "";
-	}
-	public Object initialValue() {
-		return NullReference.NULL;
-	}
+    private int structRef;
+    public RefType(int structRef) {
+	this.structRef = structRef;
+    }
+    public byte[] getBytecode() {
+	byte[] res = new byte[]{1, 0, 0};
+	NumberConversion.shortToByteArray(res, 1, (short)this.structRef);
+	return res;
+    }
+    public String toString(Interpreter interpreter) {
+	return "" + interpreter.structs[this.structRef].name + "";
+    }
+    public Object initialValue() {
+	return NullReference.NULL;
+    }
 }
--- src/bytecode/type/VoidType.java
+++ src/bytecode/type/VoidType.java
@@ -3,15 +3,15 @@ package bytecode.type;
 import runtime.Interpreter;
 
 public class VoidType extends CodeType {
-	public static VoidType TYPE = new VoidType();
-	private VoidType(){}
-	public byte[] getBytecode() {
-		return new byte[]{0};
-	}
-	public String toString(Interpreter interpreter) {
-		return "void";
-	}
-	public Object initialValue() {
-		return null;
-	}
+    public static VoidType TYPE = new VoidType();
+    private VoidType(){}
+    public byte[] getBytecode() {
+	return new byte[]{0};
+    }
+    public String toString(Interpreter interpreter) {
+	return "void";
+    }
+    public Object initialValue() {
+	return null;
+    }
 }
