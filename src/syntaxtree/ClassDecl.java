7b216eee08410a5094e1b5a2c350ad51e541e2a8
Martin Steffen <msteffen@ifi.uio.no>
2024-02-13 15:13:33 +0100

    --

--- src/syntaxtree/ClassDecl.java
+++ src/syntaxtree/ClassDecl.java
@@ -1,6 +1,6 @@
 package syntaxtree;  // This is just an inspirational example, probably 
                      // there will be no class ceclaration class, as 
-                     // compila 23 does not support class declarations.
+                     // compila 24 does not support class declarations.
 public class ClassDecl {
 
     String name;
